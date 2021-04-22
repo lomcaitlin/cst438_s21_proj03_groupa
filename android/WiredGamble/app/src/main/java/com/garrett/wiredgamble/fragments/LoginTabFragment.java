9781523632a3db5bd22a95215d5f8e89556e091b
Ty <tjharo2@gmail.com>
2021-04-22 11:18:58 -0700

    Login Working

--- android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/fragments/LoginTabFragment.java
+++ android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/fragments/LoginTabFragment.java
@@ -1,14 +1,30 @@
 package com.garrett.wiredgamble.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.garrett.wiredgamble.MainActivity;
 import com.garrett.wiredgamble.R;
+import com.parse.FindCallback;
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +32,11 @@ import com.garrett.wiredgamble.R;
  * create an instance of this fragment.
  */
 public class LoginTabFragment extends Fragment {
+
+    EditText etUsername;
+    EditText etPassword;
+    EditText etPasswordConf;
+    Button button_login;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,5 +83,29 @@ public class LoginTabFragment extends Fragment {
                               Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login_tab, container, false);
+    }
+
+    @Override
+    public void onViewCreated (@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        etUsername = view.findViewById(R.id.etUsername);
+        etPassword = view.findViewById(R.id.etPassword);
+        button_login = view.findViewById(R.id.button_login);
+
+        button_login.setOnClickListener(v -> {
+            ParseUser.logInInBackground(etUsername.getText().toString().trim(), etPassword.getText().toString().trim(), new LogInCallback() {
+                @Override
+                public void done(ParseUser user, ParseException e) {
+                    if (user != null) {
+                        // log in the user
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(getContext(), "User Not Found", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        });
     }
 }
