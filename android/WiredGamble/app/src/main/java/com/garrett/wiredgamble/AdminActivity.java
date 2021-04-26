03dcefe501eb663ae85a981178d65598349e7f85
lomcaitlin <lomcaitlin@gmail.com>
2021-04-25 23:34:35 -0700

    Update menu to include Home & Admin

--- android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/AdminActivity.java
+++ android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/AdminActivity.java
@@ -1,8 +1,15 @@
 package com.garrett.wiredgamble;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class AdminActivity extends AppCompatActivity {
 
@@ -10,5 +17,51 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+        if (userIsAdmin()) {
+            menu.findItem(R.id.menu_admin).setVisible(true);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            case R.id.menu_admin:
+                startActivity(new Intent(this, AdminActivity.class));
+                return true;
+            case R.id.logout_button:
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                // close the login activity (to remove the back arrow)
+                this.finish();
+                return true;
+                /*
+            case R.id.edit_profile_button:
+                @Breanna -> uncomment this and change the below line of code to the activity that you create for the profile
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                // close the login activity (to remove the back arrow)
+                this.finish();
+                return true;
+                */
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean userIsAdmin() {
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null && currentUser.getBoolean("isAdmin")) {
+            return true;
+        }
+        return false;
     }
 }
