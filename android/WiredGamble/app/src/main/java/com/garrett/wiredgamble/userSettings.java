019fdf53236ee52fbb09bbf0ae56791541f849f1
Breanna Holloman <breannafholloman@gmail.com>
2021-04-27 13:21:47 -0700

    working on the update settings

--- android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/userSettings.java
+++ android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/userSettings.java
@@ -3,12 +3,22 @@ package com.garrett.wiredgamble;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class userSettings extends AppCompatActivity {
-
+    EditText newUser;
+    EditText newPw;
+    EditText pwConf;
+    Button update;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        newUser = findViewById(R.id.newUsername);
+        newPw =  findViewById(R.id.newPassword);
+        pwConf = findViewById(R.id.passwordConf);
+
     }
 }
