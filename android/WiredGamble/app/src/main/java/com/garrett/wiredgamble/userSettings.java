74cb91ddc69e85c1220c059774ff14970b84eab2
Breanna Holloman <breannafholloman@gmail.com>
2021-04-27 13:50:52 -0700

    User settings essentially done just need to test and debug results

--- android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/userSettings.java
+++ android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/userSettings.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class userSettings extends AppCompatActivity {
     EditText newUser;
@@ -15,10 +19,35 @@ public class userSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
-
+        ParseUser currentUser = ParseUser.getCurrentUser();
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
         newUser = findViewById(R.id.newUsername);
         newPw =  findViewById(R.id.newPassword);
         pwConf = findViewById(R.id.passwordConf);
+        update = findViewById(R.id.updateBtn);
+
+        update.setOnClickListener(view -> {
+            String username = newUser.getText().toString().trim();
+            String password = newPw.getText().toString().trim();
+            String passwordConf = pwConf.getText().toString().trim();
+            if(currentUser != null) {
+                if(query.whereEqualTo("username", username)!= null) {
+                    Toast.makeText(this, "Username Taken", Toast.LENGTH_SHORT).show();
+                }else {
+                    currentUser.put("username", username);
+                    Toast.makeText(this, "Save Successful", Toast.LENGTH_SHORT).show();
+                }
+                if (!password.equals(passwordConf)) {
+                    Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    currentUser.put("password", password);
+                }
+            }
+        });
+
+
+
 
     }
 }
