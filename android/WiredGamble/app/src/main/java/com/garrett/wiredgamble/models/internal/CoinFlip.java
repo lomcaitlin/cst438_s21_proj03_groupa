e959a225b8ae042697a95344659ecd1d5d1f94e7
Breanna Holloman <breannafholloman@gmail.com>
2021-05-09 13:55:08 -0700

    starting coin flip activity

---
+++ android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/models/internal/CoinFlip.java
@@ -0,0 +1,16 @@
+package com.garrett.wiredgamble.models.internal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.garrett.wiredgamble.R;
+
+public class CoinFlip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_flip);
+    }
+}
