d48ef750ac38e1e2f8fc3a129bad161f083bb177
Breanna Holloman <breannafholloman@gmail.com>
2021-05-13 12:05:55 -0700

    added function

--- android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/models/internal/CoinFlip.java
+++ android/WiredGamble/app/src/main/java/com/garrett/wiredgamble/models/internal/CoinFlip.java
@@ -11,6 +11,8 @@ import com.parse.ParseUser;
 
 import org.parceler.Parcel;
 
+import java.util.HashMap;
+
 @Parcel
 public class CoinFlip extends PlayableGame{
     private static final Fragment sFragment = new RouletteFragment();
@@ -40,7 +42,35 @@ public class CoinFlip extends PlayableGame{
 
         return placedBet;
     }
+    @Override
+    public char play(PlacedBet placedBet, HashMap<Character, Payout> payoutMap, char selected) {
+        double outcome = mRandom.nextDouble();
+        double bound = 0.0;
+        char winner = 'a';
 
+        for (Character c: payoutMap.keySet()) {
+            bound += payoutMap.get(c).getOdds();
+            if (outcome <= bound) {
+                winner = c;
+                break;
+            }
+        }
+
+        if (winner == selected) {
+            ParseUser user = placedBet.getUser();
+            double balance = user.getDouble("balance");
+            user.put("balance",
+                    balance + placedBet.getBet() * placedBet.getPayout().getMultiplier());
+            user.saveInBackground();
+
+            placedBet.setStatus(PlacedBetStatus.COMPLETED_WON);
+        } else {
+            placedBet.setStatus(PlacedBetStatus.COMPLETED_LOST);
+        }
+        placedBet.saveInBackground();
+
+        return winner;
+    }
 
 
 }
